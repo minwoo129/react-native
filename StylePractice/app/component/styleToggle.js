33b520237db62c83d49077ca4241e1c9ff772fea
minwoo129 <73742422+minwoo129@users.noreply.github.com>
2020-12-01 10:40:43 +0900

    stylepractice

---
+++ StylePractice/app/component/styleToggle.js
@@ -0,0 +1,45 @@
+import { StyleSheet } from 'react-native';
+
+export const Colors = {
+    dark: 'black',
+    light: 'white'
+};
+
+const baseContainerStyles = {
+    flex: 1,
+    justifyContent: 'center'
+};
+
+const baseBoxStyles = {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 2,
+    height: 150,
+    width: 150
+}
+
+const lightStyleSheet = StyleSheet.create({
+    container: {
+        ...baseContainerStyles,
+        backgroundColor: Colors.light
+    },
+    box: {
+        ...baseBoxStyles,
+        borderColor: Colors.dark
+    }
+});
+
+const darkStyleSheet = StyleSheet.create({
+    container: {
+        ...baseContainerStyles,
+        backgroundColor: Colors.dark
+    },
+    box: {
+        ...baseBoxStyles,
+        borderColor: Colors.light
+    }
+});
+
+export default function getStyleSheet(useDarkTheme) {
+    return useDarkTheme ? darkStyleSheet : lightStyleSheet;
+}
---
+++ StylePractice/index.js
@@ -0,0 +1,8 @@
+/**
+ * @format
+ */
+import React from 'react';
+import {AppRegistry} from 'react-native';
+import App from './app/App';
+const StylePractice = () => <App />;
+AppRegistry.registerComponent('StylePractice', () => App);
